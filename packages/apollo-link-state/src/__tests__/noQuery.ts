7587cf9d27c9e7066d8888c778b6961f88a3d30c
Sashko Stubailo <sashko@meteor.com>
2017-12-12 18:00:32 -0800

    Add writeData method to cache

--- packages/apollo-link-state/src/__tests__/noQuery.ts
+++ packages/apollo-link-state/src/__tests__/noQuery.ts
@@ -1,7 +1,12 @@
+import gql from 'graphql-tag';
 import { print } from 'graphql/language/printer';
 import { parse } from 'graphql/language/parser';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { queryFromPojo, fragmentFromPojo } from '../utils';
+
+import { withClientState, ApolloCacheClient } from '../';
 
 describe('writing data with no query', () => {
   describe('converts a JavaScript object to a query correctly', () => {
@@ -73,6 +78,43 @@ describe('writing data with no query', () => {
           }),
         ),
       ).toMatchSnapshot();
+    });
+  });
+
+  describe('writeData on cache', () => {
+    it('lets you write to the cache with a mutation', () => {
+      const query = gql`
+        {
+          field @client
+        }
+      `;
+
+      const mutation = gql`
+        mutation start {
+          start @client
+        }
+      `;
+
+      const local = withClientState({
+        Mutation: {
+          start: (_, $, { cache }: { cache: ApolloCacheClient }) => {
+            cache.writeData({ data: { field: 1 } });
+            return { start: true };
+          },
+        },
+      });
+
+      const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        link: local,
+      });
+
+      return client
+        .mutate({ mutation })
+        .then(() => client.query({ query }))
+        .then(({ data }) => {
+          expect({ ...data }).toEqual({ field: 1 });
+        });
     });
   });
 });
