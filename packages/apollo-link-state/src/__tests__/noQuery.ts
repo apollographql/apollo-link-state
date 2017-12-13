a91cb0c6ced7df03156ec31a27c187c1d6a18a31
Sashko Stubailo <sashko@meteor.com>
2017-12-12 17:27:02 -0800

    Automatically generate queries from objects, part 1

---
+++ packages/apollo-link-state/src/__tests__/noQuery.ts
@@ -0,0 +1,40 @@
+import { print } from 'graphql/language/printer';
+import { parse } from 'graphql/language/parser';
+
+import { documentFromPojo } from '../utils';
+
+describe('writing data with no query', () => {
+  describe('converts a JavaScript object to a query correctly', () => {
+    it('basic', () => {
+      expect(
+        print(
+          documentFromPojo({
+            number: 5,
+            bool: true,
+            bool2: false,
+            undef: undefined,
+            nullField: null,
+            str: 'string',
+          }),
+        ),
+      ).toMatchSnapshot();
+    });
+
+    it('nested', () => {
+      expect(
+        print(
+          documentFromPojo({
+            number: 5,
+            bool: true,
+            nested: {
+              bool2: false,
+              undef: undefined,
+              nullField: null,
+              str: 'string',
+            },
+          }),
+        ),
+      ).toMatchSnapshot();
+    });
+  });
+});
