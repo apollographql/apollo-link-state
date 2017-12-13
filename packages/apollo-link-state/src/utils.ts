7680c27e217508f60f404f3fc94f89a862ea9d2d
Sashko Stubailo <sashko@meteor.com>
2017-12-12 17:27:02 -0800

    Automatically generate queries from objects, part 1

--- packages/apollo-link-state/src/utils.ts
+++ packages/apollo-link-state/src/utils.ts
@@ -1,10 +1,18 @@
-import { DocumentNode, DirectiveNode } from 'graphql';
+import {
+  DocumentNode,
+  DirectiveNode,
+  OperationDefinitionNode,
+  SelectionSetNode,
+  FieldNode,
+} from 'graphql';
 
 import {
   // getDirectives,
   checkDocument,
   removeDirectivesFromDocument,
 } from 'apollo-utilities';
+
+import { select } from 'async';
 
 const connectionRemoveConfig = {
   test: (directive: DirectiveNode) => directive.name.value === 'client',
@@ -29,4 +37,70 @@ export function removeClientSetsFromDocument(
   // caching
   removed.set(query, docClone);
   return docClone;
+}
+
+export function documentFromPojo(obj: any): DocumentNode {
+  const op: OperationDefinitionNode = {
+    kind: 'OperationDefinition',
+    operation: 'query',
+    name: {
+      kind: 'Name',
+      value: 'GeneratedClientQuery',
+    },
+    selectionSet: selectionSetFromObj(obj),
+  };
+
+  const out: DocumentNode = {
+    kind: 'Document',
+    definitions: [op],
+  };
+
+  return out;
+}
+
+function selectionSetFromObj(obj) {
+  if (
+    typeof obj === 'number' ||
+    typeof obj === 'boolean' ||
+    typeof obj === 'string' ||
+    typeof obj === 'undefined' ||
+    obj === null
+  ) {
+    // No selection set here
+    return null;
+  }
+
+  if (Array.isArray(obj)) {
+    // GraphQL queries don't include arrays
+    return selectionSetFromObj(obj[0]);
+  }
+
+  // Now we know it's an object
+  const selections: FieldNode[] = [];
+
+  Object.keys(obj).forEach(key => {
+    const field: FieldNode = {
+      kind: 'Field',
+      name: {
+        kind: 'Name',
+        value: key,
+      },
+    };
+
+    // Recurse
+    const nestedSelSet: SelectionSetNode = selectionSetFromObj(obj[key]);
+
+    if (nestedSelSet) {
+      field.selectionSet = nestedSelSet;
+    }
+
+    selections.push(field);
+  });
+
+  const selectionSet: SelectionSetNode = {
+    kind: 'SelectionSet',
+    selections,
+  };
+
+  return selectionSet;
 }
