052681f732835c1f1f15bc80906f572f775a1721
Peggy Rayzis <peggyrayzis@gmail.com>
2017-12-20 10:42:28 -0500

    Changed example to device API instead of REST

--- examples/async/src/index.js
+++ examples/async/src/index.js
@@ -6,11 +6,11 @@ import { withClientState } from 'apollo-link-state';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './containers/App';
-import schema from './schema';
+import App from './components/App';
+import resolverMap from './resolvers';
 
 const client = new ApolloClient({
-  link: withClientState(schema),
+  link: withClientState(resolverMap),
   cache: new InMemoryCache(),
 });
 
