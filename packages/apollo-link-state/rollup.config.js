4470407cd88bd4d1b38ed041031f90d21b4ede4a
Evans Hauser <evanshauser@gmail.com>
2018-02-08 16:54:05 -0800

    update rollup and added rollup to npm run watch command

--- packages/apollo-link-state/rollup.config.js
+++ packages/apollo-link-state/rollup.config.js
@@ -1,12 +1,26 @@
-export default {
-  entry: 'lib/index.js',
-  dest: 'lib/bundle.umd.js',
-  format: 'umd',
-  sourceMap: true,
-  moduleName: 'errorLink',
-  exports: 'named',
-  onwarn,
+const globals = {
+  // Apollo
+  'apollo-client': 'apollo.core',
+  'apollo-cache': 'apolloCache.core',
+  'apollo-link': 'apolloLink.core',
+  'apollo-utilities': 'apollo.utilities',
+
+  'graphql-anywhere/lib/async': 'async',
 };
+
+export default {
+  input: 'lib/index.js',
+  output: {
+    file: 'lib/bundle.umd.js',
+    format: 'umd',
+    name: 'errorLink',
+    exports: 'named',
+    sourcemap: true,
+    globals,
+  },
+  external: Object.keys(globals),
+  onwarn,
+}
 
 function onwarn(message) {
   const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
